a6771af9a9cd95485e6a4244695d8311db6ba55c
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-04-15 22:33:02 -0300

    adjusts

--- 001 - variaveis/02 - numeros.py
+++ 001 - variaveis/02 - numeros.py
@@ -1,9 +1,12 @@
 #Escreva um programa que receba 2 valores do tipo inteiro x e y, e calcule o valor de z: z = (𝑥² + 𝑦²) / (𝑥−𝑦)²
-X = int(input("Número X: "))
-Y = int(input("Número Y: "))
-Z = (X**2 + Y**2) / (X-Y)**2
-print("Resultado: ", Z, "\n\n")
+X = float(input("Número X: "))
+Y = float(input("Número Y: "))
+try:
+    Z = (X**2 + Y**2) / (X-Y)**2
+    print("Resultado: ", Z)
+except:
+    print('Impossível calcular')
 
 #Escreva  um  programa  que  receba  o  salário  de  um  funcionário  (float),  e  retorne  o  resultado  do novo salário com reajuste de 35%
-S = float(input("Salário atual: "))
-print('Salário ajustado: ', S + (S*.35))
+S = float(input("\nSalário atual: "))
+print('Salário ajustado: %.2f' %(S + (S*.35)))
--- 002 - estruturas de controle/03 - for.py
+++ 002 - estruturas de controle/03 - for.py
@@ -1,6 +1,6 @@
 # Escreva um programa para encontrar a soma S = 3 + 6 + 9 + .... + 333.
 S = 0
-for C in range(3, 333, 3): S += C
+for C in range(3, 334, 3): S += C
 print('Soma de 3 + 6 + 9 + .... + 333:', S)
 
 # Escreva um programa que leia um número de 1 a 10, e mostre a tabuada desse número.
