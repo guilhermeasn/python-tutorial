33cc7f7a4e7756bbd4a08663aa0a25ce0aa31103
Guilherme Neves <guilhermeasn@yahoo.com.br>
2023-04-28 06:42:29 -0300

    Lower

--- 002 - estruturas/04 - match.py
+++ 002 - estruturas/04 - match.py
@@ -1,15 +1,15 @@
 lang = input("What's the programming language you want to learn? ")
 
-match lang:
-    case "JavaScript":
+match lang.lower():
+    case "javascript":
         print("You can become a web developer.")
-    case "Python":
+    case "python":
         print("You can become a Data Scientist")
-    case "PHP":
+    case "php":
         print("You can become a backend developer")
-    case "Solidity":
+    case "solidity":
         print("You can become a Blockchain developer")
-    case "Java":
+    case "java":
         print("You can become a mobile app developer")
     case _:
         print("The language doesn't matter, what matters is solving problems.")
